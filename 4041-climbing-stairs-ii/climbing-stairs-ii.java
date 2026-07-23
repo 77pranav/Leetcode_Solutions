86e4a30870a40700c567411d57ae79685364f015
77pranav <145129645+77pranav@users.noreply.github.com>
2026-07-23 22:03:46 +0530

    Time: 4 ms (94.93%) | Memory: 110.3 MB (91.69%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,44 +1,26 @@
 class Solution {
     public int climbStairs(int n, int[] costs) {
-        return recursionMemo(n,costs,new Integer[n+1]);
+        return spaceOptimized(n,costs);
     }
-    public int recursion(int n,int[] costs){
-        if(n<1) return n;
-        int cost1=Integer.MAX_VALUE;
-        int cost2=Integer.MAX_VALUE;
-        int cost3=Integer.MAX_VALUE;
-        if(n>=1){
-            int power=(int)Math.pow(n-(n-1),2);
-            cost1=recursion(n-1,costs)+costs[n-1]+power;
+    public int spaceOptimized(int n,int[] costs){
+        int third=0;
+        int second=Integer.MAX_VALUE;
+        int first=second;
+        int answer=-1;
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                answer=Math.min(third+costs[i-1]+1,Math.min(second,first));
+            }
+            if(i==2){
+                answer=Math.min(third+costs[i-1]+1,Math.min(second+costs[i-1]+4,first));
+            }
+            if(i>=3){
+                answer=Math.min(third+costs[i-1]+1,Math.min(second+costs[i-1]+4,first+costs[i-1]+9));
+            }
+            first=second;
+            second=third;
+            third=answer;
         }
-        if(n>=2){
-            int power=(int)Math.pow(n-(n-2),2);
-            cost2=recursion(n-2,costs)+costs[n-1]+power;
-        }
-        if(n>=3){
-            int power=(int)Math.pow(n-(n-3),2);
-            cost3=recursion(n-3,costs)+costs[n-1]+power;
-        }
-        return Math.min(cost1,Math.min(cost2,cost3));
-    }
-    public int recursionMemo(int n,int[] costs,Integer[] dp){
-        if(n<1) return n;
-        if(dp[n]!=null) return dp[n];
-        int cost1=Integer.MAX_VALUE;
-        int cost2=Integer.MAX_VALUE;
-        int cost3=Integer.MAX_VALUE;
-        if(n>=1){
-            int power=(int)Math.pow(n-(n-1),2);
-            cost1=recursionMemo(n-1,costs,dp)+costs[n-1]+power;
-        }
-        if(n>=2){
-            int power=(int)Math.pow(n-(n-2),2);
-            cost2=recursionMemo(n-2,costs,dp)+costs[n-1]+power;
-        }
-        if(n>=3){
-            int power=(int)Math.pow(n-(n-3),2);
-            cost3=recursionMemo(n-3,costs,dp)+costs[n-1]+power;
-        }
-        return dp[n]=Math.min(cost1,Math.min(cost2,cost3));
+        return answer;
     }
 }
