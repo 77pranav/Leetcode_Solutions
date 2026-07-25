043f58207b2baf931d5e5f48e5710011ba7a9462
77pranav <145129645+77pranav@users.noreply.github.com>
2026-07-25 23:07:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (13.19%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+    //    return recursionMemo(m,n,new Integer[m+1][n+1]);
+        return tabulation(m,n); 
+    }
+    public int recursion(int m,int n){
+        if(m<=1 && n<=1) return 1;
+        if(m<=0 || n<=0) return 0;
+        int down=recursion(m-1,n);
+        int right=recursion(m,n-1);
+        return down+right;
+    }
+    public int recursionMemo(int m,int n,Integer dp[][]){
+        if(m<=1 && n<=1) return 1;
+        if(m<=0 || n<=0) return 0;
+        if(dp[m][n] != null) return dp[m][n];
+        int down=recursionMemo(m-1,n,dp);
+        int right=recursionMemo(m,n-1,dp);
+        return dp[m][n]=down+right;
+    }
+    public int tabulation(int m,int n){
+        int[][] dp=new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,1);
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
