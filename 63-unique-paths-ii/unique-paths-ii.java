29990d14f99e0dcf120938d8aefa7e12f9b0a74d
77pranav <145129645+77pranav@users.noreply.github.com>
2026-07-26 10:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (26.79%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -4,32 +4,33 @@ class Solution {
         int n=obstacleGrid[0].length-1;
         // return recursion(m,n,obstacleGrid);
         // return recursionMemo(m,n,obstacleGrid,new Integer[m+1][n+1]);
-        return tabulation(m,n,obstacleGrid);
+        // return tabulation(m,n,obstacleGrid);
+        return spaceOptimized(m,n,obstacleGrid);
     }
-    
-    public int tabulation(int m,int n,int[][] obstacleGrid){
-        int[][] dp=new int[m+1][n+1];
-        for(int i=0;i<=m;i++){
-            if(obstacleGrid[i][0]==0){
-                dp[i][0]=1;
+    public int spaceOptimized(int m,int n,int[][] obstacleGrid){
+        int[] col=new int[n+1];
+        for(int i=0;i<=n;i++){
+            if(obstacleGrid[0][i]==0){
+                col[i]=1;
             }else{
                 break;
             }
         }
-        for(int j=0;j<=n;j++){
-            if(obstacleGrid[0][j]==0){
-                dp[0][j]=1;
-            }else{
-                break;
-            }
-        }
+        int last=col[n];
         for(int i=1;i<=m;i++){
+            int row=(obstacleGrid[i][0]==0) ? col[0] : 0;
+            col[0]=row;
+            last=row;
             for(int j=1;j<=n;j++){
                 if(obstacleGrid[i][j]==0){
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                    last=row+col[j];
+                }else{
+                    last=0;
                 }
+                col[j]=last;
+                row=last;
             }
         }
-        return dp[m][n];
+        return last;
     }
 }
